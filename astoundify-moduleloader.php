ed7f3a499c11fb9bd5950fea3430782506cb989d
Spencer Finnell <spencerfinnell@gmail.com>
2016-11-15 10:08:17 -0500

    restructure add composer

--- astoundify-moduleloader.php
+++ astoundify-moduleloader.php
@@ -3,13 +3,10 @@
  * Plugin Name: Astoundify Module Loader
  * Plugin URI: https://astoundify.com
  * Description: Provide a quick way to automatically modules and dependencies.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Astoundify
  * Author URI: http://astoundify.com
  */
 
-/**
- * See the `/examples` directory.
- */
-// require_once( dirname( __FILE__ ) . '/examples/example-1/example-1.php' );
-// require_once( dirname( __FILE__ ) . '/examples/example-2/example-2.php' );
+// require app
+require_once( dirname( __FILE__ ) . '/app/astoundify-moduleloader.php' );
